181cf57a32b265e66321092369fc67a45cdfcf10
SimaDovakin <simadovakin999@gmail.com>
2022-08-23 13:51:08 +0300

    Created models for the queues app.

--- virtual_queue/queues/admin.py
+++ virtual_queue/queues/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Queue,
+    Participant,
+    Location,
+    Country,
+    Region,
+    City
+)
+
+
+class QueueAdmin(admin.ModelAdmin):
+    model = Queue
+
+
+class ParticipantAdmin(admin.ModelAdmin):
+    model = Participant
+
+
+class LocationAdmin(admin.ModelAdmin):
+    model = Location
+
+
+class CountryAdmin(admin.ModelAdmin):
+    model = Country
+
+
+class RegionAdmin(admin.ModelAdmin):
+    model = Region
+
+
+class CityAdmin(admin.ModelAdmin):
+    model = City
+
+
+admin.site.register(Queue, QueueAdmin)
+admin.site.register(Participant, ParticipantAdmin)
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Country, CountryAdmin)
+admin.site.register(Region, RegionAdmin)
+admin.site.register(City, CityAdmin)
--- virtual_queue/queues/models.py
+++ virtual_queue/queues/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Queue(models.Model):
+    """
+    Stores the data of a particular queue.
+    """
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='queues')
+    location = models.ForeignKey('Location', on_delete=models.CASCADE, related_name='queues')
+    name = models.CharField(max_length=256)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.creator.username})"
+
+
+class Participant(models.Model):
+    """
+    Binds User and Queue models. Stores the order number of users in
+    a particular queue.
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='participations')
+    queue = models.ForeignKey(Queue, on_delete=models.CASCADE, related_name='participants')
+    queue_order = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} ({self.queue.name})"
+
+
+class Location(models.Model):
+    """
+    Stores the country, city, region, and address string.
+    """
+    country = models.ForeignKey('Country', on_delete=models.CASCADE, related_name='locations')
+    region = models.ForeignKey('Region', on_delete=models.CASCADE, related_name='locations')
+    city = models.ForeignKey('City', on_delete=models.CASCADE, related_name='locations')
+    address = models.CharField(max_length=500)
+
+    def __str__(self):
+        return f"{self.country.name}, {self.city.name}, {self.address}"
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.name
+
+
+class Region(models.Model):
+    name = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.name
+
+
+class City(models.Model):
+    name = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.name
