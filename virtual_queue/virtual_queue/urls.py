88a254917ff873ba6431f202a19e0b1173f4be6d
SimaDovakin <simadovakin999@gmail.com>
2023-07-02 13:46:35 +0300

    [drf-integration]: - Added Django REST framework and drf_yasg to the project dependencies. - Included Swagger endpoint for the project.

--- virtual_queue/virtual_queue/urls.py
+++ virtual_queue/virtual_queue/urls.py
@@ -1,8 +1,25 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+
+swagger_view = get_schema_view(
+    openapi.Info(
+        title="Virtual Queue",
+        default_version="v1"
+    ),
+    public=True
+)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('queues.urls'))
+    path('', include('queues.urls')),
+
+    re_path(
+        r'^docs/$',
+        swagger_view.with_ui("swagger", cache_timeout=0),
+        name='api-docs'
+    )
 ]
